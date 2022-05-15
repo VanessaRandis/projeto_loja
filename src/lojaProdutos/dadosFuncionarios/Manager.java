ee544546e2741fa9d3045cadaa2d5813d0cdecdd
Vanessa <vanessa.randis@gmail.com>
2022-05-15 18:28:19 -0300

    alterações em metodos filho

--- src/lojaProdutos/dadosFuncionarios/Manager.java
+++ src/lojaProdutos/dadosFuncionarios/Manager.java
@@ -1,10 +1,21 @@
 package lojaProdutos.dadosFuncionarios;
 
+import lojaProdutos.dados.Address;
+
 public class Manager extends Funcionarios {
 	
 	private int senha;
 	
+	public Manager() {
+		super();
+	}
 	
+	public Manager(String cpf, String name, Address address, String id, double salary, int senha) {
+		super(cpf, name, address, id, salary);
+		this.senha = senha;
+	}
+
+
 	public boolean autentica(int senha) {
 		if(this.senha== senha) {
 		return true;
@@ -18,12 +29,14 @@ public class Manager extends Funcionarios {
 		
 	}
 
-
+	@Override
 	public double getBonification() {
 		
 		return super.getBonification() + super.getSalary();
 	}
 	
+	
+	@Override
 	public double getTotalSalary() {
 		
 		return this.getBonification()+super.getSalary();
